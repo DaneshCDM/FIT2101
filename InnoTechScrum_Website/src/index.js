3ef5a95697cec92aeb99ed2c7f66d64e4bf6249c
Toh Xi Heng <xtoh0004@student.monash.edu>
2023-09-12 17:34:22 +0800

    addtask clickable, code some for popup but wrong

--- InnoTechScrum_Website/src/index.js
+++ InnoTechScrum_Website/src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://innotechscrum-default-rtdb.asia-southeast1.firebasedatabase.app.js";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "https://innotechscrum-default-rtdb.asia-southeast1.firebasedatabase.app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 // Assuming you have a function to fetch data from the database
 function fetchDataFromDatabase(searchQuery) {
