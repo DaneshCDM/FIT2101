fdd501f18102b221ec788b1546291117af6aa032
wcha0099 <wcha0099@student.monash.edu>
2023-09-13 00:30:21 +0800

    Added openPopUp and closePopUp function

--- InnoTechScrum_Website/src/index.js
+++ InnoTechScrum_Website/src/index.js
@@ -29,7 +29,19 @@ function fetchDataFromDatabase(searchQuery) {
   const data = `Data for search query: ${searchQuery}`;
   document.getElementById('result-container').textContent = data;
 }
-
+function openPopUp(){
+  const popup = document.getElementById("popup");
+  const popupContentText = "This is the popup content";
+  const popupContent = document.createElement("div");
+  popupContent.textContent = popupContentText;
+  popup.appendChild(popupContent);
+  popup.style.display="block";
+}
+function closePopUp(){
+  const popup = document.getElementById("popup");
+  popup.style.display="none";
+  popup.innerHTML ="";
+}
 document.getElementById('search-bar').addEventListener('keyup', function(event) {
   if (event.key === 'Enter') {
       const searchQuery = event.target.value;
