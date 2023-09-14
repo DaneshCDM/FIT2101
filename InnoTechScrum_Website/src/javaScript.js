1f1a8647f37aba782c1c6179723411a6f0f5b535
wcha0099 <wcha0099@student.monash.edu>
2023-09-14 23:15:38 +0800

    modal popup for addtask, create Task not working

--- InnoTechScrum_Website/src/javaScript.js
+++ InnoTechScrum_Website/src/javaScript.js
@@ -1,13 +1,88 @@
 function openPopUp(){
-    const popup = document.getElementById("popup");
-    const popupContentText = "This is the popup content";
-    const popupContent = document.createElement("div");
-    popupContent.textContent = popupContentText;
-    popup.appendChild(popupContent);
-    popup.style.display="block";
-  }
+  const popup = document.getElementById("popup");
+  const popupContentText = "This is the popup content";
+  const popupContent = document.createElement("div");
+  popupContent.textContent = popupContentText;
+  popup.appendChild(popupContent);
+  popup.style.display="block";
+}
 function closePopUp(){
 const popup = document.getElementById("popup");
 popup.style.display="none";
 popup.innerHTML ="";
 }
+
+// function openPopUp(){
+//     const overlay =document.getElementById("overlay")
+//     const popup = document.getElementById("popup");
+//     if (overlay&&popup){
+//       const popupContentText = "This is the popup content";
+//       const popupContent = document.createElement("div");
+//       popupContent.textContent = popupContentText;
+//       popup.appendChild(popupContent);
+//       popup.style.display="block";
+//       overlay.style.display="block";
+//     }
+//     const popupContentText = "This is the popup content";
+//     const popupContent = document.createElement("div");
+//     popupContent.textContent = popupContentText;
+//     popup.appendChild(popupContent);
+//     popup.style.display="block";
+//   }
+// function closePopUp(){
+//     const overlay =document.getElementById("overlay")
+//     const popup = document.getElementById("popup");
+//     if(overlay&&popup){
+//       popup.style.display="none";
+//       overlay.style.display="none";
+//       popup.innerHTML ="";
+//     }
+//     popup.style.display="none";
+//     popup.innerHTML ="";
+// }
+// function openPopUpAndShowOverlay() {
+//   openPopUp();
+//   showOverlay();
+// }
+
+// function showOverlay() {
+//   const overlay = document.getElementById("overlay");
+//   overlay.style.display = "block";
+// }
+// function closePopUpAndHideOverlay() {
+//   closePopUp();
+//   hideOverlay();
+// }
+
+// function hideOverlay() {
+//   const overlay = document.getElementById("overlay");
+//   overlay.style.display = "none";
+// }
+// document.addEventListener("DOMContentLoaded", function() {
+//   const checkPopUp = document.getElementById("open-button").addEventListener("click", openPopUp);
+//   const checkClosePopUp = document.getElementById("close-button").addEventListener("click", closePopUp);
+// });
+// function openModal(){
+//   $('modalCase').modal('show');
+
+// }
+// function callCloseModal(){
+//   modal.style.display="none";
+// }
+// function closeModal(){
+//   //Data
+//   var modal = document.getElementById("modalCase");
+//   var closeButton=document.getElementsByClassName("close")[0];
+//   for(let closeBtn of closeButton){
+//     closeBtn.onclick =callCloseModal;
+//   }
+//   console.log('closeModal function called');
+//   $('#modalCase').modal('hide');
+//   // closeButton.onclik=function(){
+//   //   modal.style.display="none";
+//   //   $('#modalCase').modal('hide');
+//   // }
+// }
+// window.closeModal = function(){
+//   parent.$('#modelCase').modal('hide');
+// };
