205c8cde53cb38fef6c559deffc186319e120725
Andreas-03 <akok0011@student.monash.edu>
2023-09-14 21:20:42 +0800

    modified:   InnoTechScrum_Website/src/dashboard-dark-product-backlog.css 	modified:   InnoTechScrum_Website/src/dashboard-dark-product-backlog.html 	modified:   InnoTechScrum_Website/src/javaScript.js

--- InnoTechScrum_Website/src/javaScript.js
+++ InnoTechScrum_Website/src/javaScript.js
@@ -1,13 +1,17 @@
 function openPopUp(){
-    const popup = document.getElementById("popup");
-    const popupContentText = "This is the popup content";
-    const popupContent = document.createElement("div");
-    popupContent.textContent = popupContentText;
-    popup.appendChild(popupContent);
-    popup.style.display="block";
-  }
+  const popup = document.getElementById("popup");
+  const popupContentText = "This is the popup content";
+  const popupContent = document.createElement("div");
+  popupContent.textContent = popupContentText;
+  popup.appendChild(popupContent);
+  popup.style.display="block";
+}
 function closePopUp(){
-const popup = document.getElementById("popup");
-popup.style.display="none";
-popup.innerHTML ="";
+  const popup = document.getElementById("popup");
+  popup.style.display="none";
+  popup.innerHTML ="";
+}
+
+function chooseStorypoint(){
+
 }
